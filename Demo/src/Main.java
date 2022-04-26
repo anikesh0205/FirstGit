2e34675ee24eabf1c3bcb125dcbc48841f6510f7
M1082891 <M1082891@MT-5CG1478YJS>
2022-04-26 22:15:27 +0530

    Its my Demo

---
+++ Demo/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x=3;
+		int flag=0;
+		if((x%2)==0)
+		{
+			flag=1;    //for even flag=1 & for odd flag=0
+		}
+		if (flag==1) {
+			System.out.println("Even number");
+		}
+		else {
+			System.out.println("Odd number");
+		}
+
+	}
+
+}
